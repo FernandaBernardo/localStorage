f8b289f62fefadcfc1e7c02dcfac42a7df8fae0b
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-03-04 00:27:56 -0300

    arrumando html e começando o js

---
+++ public/javascripts/script.js
@@ -0,0 +1,26 @@
+var APP = (function() {
+	var module = {};
+	module.task = document.querySelector(".task");
+	module.add = document.querySelector(".add");
+	module.list = document.querySelector(".list");
+
+	module.validate = function() {
+
+	};
+
+	module.persist = function() {
+
+	};
+
+	module.updateList = function() {
+
+	}
+
+	module.init = function() {
+
+	};
+
+	return {init: module.init};
+})();
+
+APP.init();
